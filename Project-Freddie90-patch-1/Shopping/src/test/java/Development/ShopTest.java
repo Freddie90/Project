34677d18cdd9458052475a34e2476e5fbc75e65e
LeBeatrix <47351662+LeBeatrix@users.noreply.github.com>
2019-02-13 18:57:37 +0200

    Add files via upload

---
+++ Project-Freddie90-patch-1/Shopping/src/test/java/Development/ShopTest.java
@@ -0,0 +1,15 @@
+package Development;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShopTest {
+@Test
+    public void shouldAnswerWithTrue() {
+        //double paymentDue = 0;
+        int amount = 0;
+        double itemPrice = 0;
+        Assert.assertEquals((amount*itemPrice), amount, itemPrice);
+    }
+}
+
